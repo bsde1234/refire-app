77c043dc4b4639f6c715bde11b94f0e934ec5afd
Lari Hoppula <lari@hoppula.com>
2016-04-22 20:00:30 +0300

    styles HOC now supports adding global styles with registerRule. Extended styles now get their own class instead of being merged with original, helps debugging & prevents problems with react-free-style.

--- src/components/styles.js
+++ src/components/styles.js
@@ -4,12 +4,40 @@ import hoistNonReactStatics from 'hoist-non-react-statics'
 
 export default (styles, Component) => {
   const Style = create()
+  const globalStyles = {}
 
-  function createStyles(styles) {
-    return Object.keys(styles).reduce((result, id) => {
+  function createStyles(styleProps) {
+    return Object.keys(styleProps || {}).reduce((result, id) => {
+      // register global styles when id is "registerRule"
+      if (id === "registerRule") {
+        Object.keys(styleProps[id]).forEach((rule) => {
+          if (globalStyles[rule]) {
+            // remove existing global styles
+            const existing = Object.keys(Style._children).filter((childId) => {
+              return Style._children[childId].rule === rule
+            }).forEach((ruleId) => {
+              Style.remove(Style._children[ruleId])
+            })
+          }
+          // merge new global styles with existing styles
+          globalStyles[rule] = {...globalStyles[rule], ...styleProps[id][rule]}
+          Style.registerRule(rule, globalStyles[rule])
+        })
+        return result
+      } else {
+        return {
+          ...result,
+          [id]: Style.registerStyle(styleProps[id])
+        }
+      }
+    }, {})
+  }
+
+  function concatenateClassnames(componentStyles, userStyles={}) {
+    return Object.keys(componentStyles).reduce((result, id) => {
       return {
         ...result,
-        [id]: Style.registerStyle(styles[id])
+        [id]: [componentStyles[id], userStyles[id]].filter(Boolean).join(" ")
       }
     }, {})
   }
@@ -20,18 +48,18 @@ export default (styles, Component) => {
 
     constructor(props) {
       super(props)
-      this.state = { userStyles: null }
+      this.state = { userStyles: {} }
     }
 
     componentWillMount() {
       if (this.props.styles) {
-        const userStyles = Object.keys(this.props.styles).reduce((result, id) => {
-          return {
-            ...result,
-            [id]: {...styles[id], ...this.props.styles[id]}
-          }
-        }, {})
-        this.setState({userStyles: createStyles(userStyles)})
+        this.setState({userStyles: createStyles(this.props.styles)})
+      }
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.styles !== this.props.styles) {
+        this.setState({userStyles: createStyles(nextProps.styles)})
       }
     }
 
@@ -39,7 +67,7 @@ export default (styles, Component) => {
       return (
         <Component
           {...this.props}
-          styles={{...stylesProp, ...this.state.userStyles}}
+          styles={concatenateClassnames(stylesProp, this.state.userStyles)}
         />
       )
     }
