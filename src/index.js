b018adf724dd3a021bbb8eb71746a5cb94964b89
Lari Hoppula <lari@hoppula.com>
2017-09-10 23:07:11 +0300

    Now exports RefireProvider and Bind components from refire-react
    Bindings can be now passed either as regular JS object or as JSX containing Bind components.
    
    App is now wrapped with RefireProvider so syncFirebase instance will be in the React's context.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 
-import { syncFirebase, firebaseReducer, firebaseActions } from 'refire'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistory, syncParams, routeParamsReducer } from 'react-router-redux-params'
+import { syncFirebase, firebaseReducer, firebaseActions } from "refire"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, combineReducers } from "redux"
+import thunk from "redux-thunk"
+import { Router, Route, browserHistory } from "react-router"
+import {
+  syncHistory,
+  syncParams,
+  routeParamsReducer
+} from "react-router-redux-params"
 
-import StyleContainer from './components/StyleContainer'
+import StyleContainer from "./components/StyleContainer"
 
-export { firebase } from 'refire'
+export { firebase } from "refire"
+import { RefireProvider } from "refire-react"
 export {
-  firebaseToProps, FirebaseLogin, FirebaseLogout, FirebaseOAuth, FirebaseRegistration,
-  FirebaseResetPassword, FirebaseWrite
-} from 'refire-react'
-export { connect } from 'react-redux'
-export { bindActionCreators } from 'redux'
-export { Link, IndexLink, IndexRedirect, IndexRoute, Redirect, Route, browserHistory, hashHistory } from 'react-router'
-export { routeActions } from 'react-router-redux-params'
-export { create as createStyles } from 'react-free-style'
+  firebaseToProps,
+  FirebaseLogin,
+  FirebaseLogout,
+  FirebaseOAuth,
+  FirebaseRegistration,
+  FirebaseResetPassword,
+  FirebaseWrite,
+  RefireProvider,
+  Bind
+} from "refire-react"
+export { connect } from "react-redux"
+export { bindActionCreators } from "redux"
+export {
+  Link,
+  IndexLink,
+  IndexRedirect,
+  IndexRoute,
+  Redirect,
+  Route,
+  browserHistory,
+  hashHistory
+} from "react-router"
+export { routeActions } from "react-router-redux-params"
+export { create as createStyles } from "react-free-style"
 
 const { USER_AUTHENTICATED, USER_UNAUTHENTICATED } = firebaseActions
 export { USER_AUTHENTICATED, USER_UNAUTHENTICATED }
 
-export { default as bindings } from './components/bindings'
-export { default as styles } from './components/styles'
+export { default as bindings } from "./components/bindings"
+export { default as styles } from "./components/styles"
 
 export default function refireApp({
   apiKey,
@@ -37,9 +58,8 @@ export default function refireApp({
   reducers = {},
   middleware = [],
   history = browserHistory,
-  elementId = 'app'
+  elementId = "app"
 }) {
-
   if (typeof projectId === "undefined") {
     throw new Error("refire-app: no projectId provided")
   }
@@ -49,7 +69,9 @@ export default function refireApp({
   }
 
   if (typeof url !== "undefined") {
-    throw new Error("refire-app: url is deprecated in options, use projectId & apiKey instead")
+    throw new Error(
+      "refire-app: url is deprecated in options, use projectId & apiKey instead"
+    )
   }
 
   if (typeof bindings === "undefined") {
@@ -74,26 +96,28 @@ export default function refireApp({
     })
   )
 
+  const componentBindings = React.isValidElement(bindings)
   syncParams(store, routes, history)
 
-  syncFirebase({
+  const sync = syncFirebase({
     apiKey: apiKey,
     projectId: projectId,
     store: store,
-    bindings: bindings,
+    bindings: componentBindings ? {} : bindings,
     pathParams: pathParams,
     onAuth: onAuth
   })
 
   ReactDOM.render(
-    <Provider store={store}>
-      <StyleContainer>
-        <Router history={history}>
-          {routes}
-        </Router>
-      </StyleContainer>
-    </Provider>
-    , document.getElementById(elementId)
+    <RefireProvider syncFirebase={sync}>
+      <Provider store={store}>
+        <StyleContainer>
+          {componentBindings && bindings}
+          <Router history={history}>{routes}</Router>
+        </StyleContainer>
+      </Provider>
+    </RefireProvider>,
+    document.getElementById(elementId)
   )
 
   return store
